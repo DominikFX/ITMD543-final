19db511ce07a09f65ce2c6a4949fa00bb297c8d2
Doxmin <mdaisonu@gmail.com>
2025-10-23 23:10:50 -0500

    initial landing

---
+++ mic-vault/src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>ITMD 543 Final Landing Page</h1>
+      <div className="card">
+        <p>
+          This is the landing page for my final project in the ITMD 543 course.
+          Dominik Kulawiak
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
